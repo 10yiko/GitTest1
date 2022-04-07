93080e3af0248daa32fbf6122f2a011171c28aca
yiko <15702841910@163.com>
2022-04-07 18:26:16 +0800

    first commit

--- src/main/java/com/yiko/test/GitTest.java
+++
@@ -1,5 +0,0 @@
-package com.yiko.test;
-
-public class GitTest {
-
-}
--- src/main/java/com/yiko/test/gitTest1.java
+++
@@ -1,9 +0,0 @@
-package com.yiko.test;
-
-public class gitTest1 {
-
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("yiko");
-    }
-}
