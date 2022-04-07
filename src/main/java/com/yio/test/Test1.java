dd7e4a0471257b3da789c049cb94570e8c138984
10yiko <103188594+10yiko@users.noreply.github.com>
2022-04-07 20:10:39 +0800

    pull commit

--- src/main/java/com/yio/test/Test1.java
+++ src/main/java/com/yio/test/Test1.java
@@ -9,5 +9,6 @@ public class Test1 {
         System.out.println("test");
         System.out.println("test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
